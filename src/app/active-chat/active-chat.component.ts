ee836700ef33b8d5a3b3e9389beba08c17bd948c
andrea <andrea@zipcoders-MacBook-Pro-4.local>
2017-12-28 19:31:26 -0500

    fixed message automatic refresh

--- src/app/active-chat/active-chat.component.ts
+++ src/app/active-chat/active-chat.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Message } from '../model/message';
 import { ChatService } from '../service/chat-service';
 import { Observable } from 'rxjs';
+import { TimerObservable } from "rxjs/observable/TimerObservable";
+import 'rxjs/add/operator/takeWhile';
 
 @Component({
   selector: 'app-active-chat',
@@ -11,19 +13,47 @@ import { Observable } from 'rxjs';
 export class ActiveChatComponent implements OnInit {
   public windowTitle = 'Active Chat Window';
 
-  private messages: Observable<any>;
+  // private messages: Message[] = [];
+  private messages: Observable<Message>;
 
+  private display: boolean; // whether to display info in the component
+                            // use *ngIf="display" in your html to take
+                            // advantage of this
 
-
-  constructor(private _chatService: ChatService) { }
-
-  ngOnInit(): void {
-    setInterval(
-    this.getMessages(),500);
+  private alive: boolean; // used to unsubscribe from the TimerObservable
+                          // when OnDestroy is called.
+  private interval: number;
+  constructor(private _chatService: ChatService) {
+    this.display = false;
+    this.alive = true;
+    this.interval = 500;
   }
 
-  getMessages() {
-    this.messages = this._chatService.getMessages();
+  // ngOnInit(): void {
+  //   this.getMessages();
+  //   window.setInterval(this.getMessages(),500);
+  // }
+  //
+  // getMessages() {
+  //   this._chatService.getMessages().subscribe(messages => {
+  //     this.messages = messages;
+  //     console.log(this.messages);
+  //   });
+  // }
+
+  ngOnInit() {
+    TimerObservable.create(0, this.interval)
+      .takeWhile(() => this.alive)
+      .subscribe(() => {
+        // this._chatService.getMessages().subscribe(messages => {
+        //   this.messages = messages;
+        //   console.log(this.messages);
+        // });
+        this.messages = this._chatService.getMessages();
+      });
   }
 
+  ngOnDestroy(){
+    this.alive = false; // switches your TimerObservable off
+  }
 }
---
+++ src/app/mock-message-data/mockMessageData.ts
@@ -0,0 +1,20 @@
+export const MockMessages = [
+    {
+        "messageID": 1,
+        "message": "test test test",
+        "timestamp": "Wed Dec 20 15:26:15 EST 2017",
+        "userId": 1
+    },
+    {
+        "messageID": 2,
+        "message": "test test test",
+        "timestamp": "Wed Dec 20 15:27:06 EST 2017",
+        "userId": 2
+    },
+    {
+        "messageID": 3,
+        "message": "message message not creative right now",
+        "timestamp": "Wed Dec 20 15:27:31 EST 2017",
+        "userId": 3
+    }
+]
